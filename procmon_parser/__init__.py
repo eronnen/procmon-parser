96080961e51745ddc6e83490c24e540553c44c71
Ely Ronnen <elyronnen@gmail.com>
2026-09-03 23:43:38 +0000

    bump 0.4.0 (#62)

--- procmon_parser/__init__.py
+++ procmon_parser/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.3.13"
+__version__ = "0.4.0"
 
 from procmon_parser.configuration import Column, Font, Rule, RuleAction, RuleRelation
 from procmon_parser.configuration_format import (
